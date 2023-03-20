45bb921ec3e8dae8a763d667621313d3f0286a47
JaimeP <jpatino1806@gmail.com>
2023-03-20 18:36:48 -0500

    primeros componentes en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: InicioComponent
+  },
+  {
+    path: 'servicio',
+    component: ServicioComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent} from "./inicio/inicio.component";
+import { ContactoComponent } from './contacto/contacto.component';
+import { BlogComponent } from './blog/blog.component';
+import { ServicioComponent } from './servicio/servicio.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent,
+    BlogComponent,
+    ServicioComponent
   ],
   imports: [
     BrowserModule,
